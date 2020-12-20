41605eafde21b82a344aaea569b007fe00c6bd10
Saad Rana <40745451+SaadRana@users.noreply.github.com>
2020-12-20 19:57:22 +0000

    updated about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -134,7 +134,8 @@ const About = () => {
                 <p>
                   In my spare time I love to play video games such as Valorant
                   and Genshin Impact. Working towards my dream gaming setup, RGB
-                  Everything!
+                  Everything! I love listening to music, and enjoy doing
+                  karaoke.
                 </p>
               </div>
 
